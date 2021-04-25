d7a6ad175cf71478aa9d39e48266993465402e4b
JuanPGG <juanpa2062@hotmail.com>
2021-04-25 09:48:38 -0500

    :hammer: Organizando animacion grafica barras

--- js/grafica.js
+++ js/grafica.js
@@ -7,49 +7,62 @@ window.intlTelInput(input, {
 
 
 
-const data_ = {
-  labels: ['Bonos Extranjeros', 'Criptoactivos', 'Factoring Latam y USA', 'Mercado Inmobiliario', 'Factoring Internacional', 'Divisas','Fondos Satélite'],
-  datasets: [{
-    label: '%',
-    borderColor: "#072146",
-    backgroundColor: ["#072146","#072146","#072146","#072146","#072146","#072146","#072146"],
-    borderWidth: 1,
-    radius: 0,
-    data: [5,7,9,11,13,16,27,29],
-  }]
-};
-var delayed
-const animation = {
-  onComplete: () => {
-    delayed = true;
-  },
-  delay: (context) => {
-    let delay = 0;
-    if (context.type === 'data' && context.mode === 'default' && !delayed) {
-      delay = context.dataIndex * 300 + context.datasetIndex * 100;
-    }
-    return delay;
-  },
-  
-};
-
-
-const config = {
-  responsive:true,
-  animation,
-  plugins: {
-    legend: {
-      position: 'top',
+function grafica(){
+  const data_ = {
+    labels: ['Bonos Extranjeros', 'Criptoactivos', 'Factoring Latam y USA', 'Mercado Inmobiliario', 'Factoring Internacional', 'Divisas','Fondos Satélite'],
+    datasets: [{
+      label: '%',
+      borderColor: "#072146",
+      backgroundColor: ["#072146","#072146","#072146","#072146","#072146","#072146","#072146"],
+      borderWidth: 1,
+      radius: 0,
+      data: [5,7,9,11,13,16,27,29],
+    }]
+  };
+  var delayed
+  const animation = {
+    onComplete: () => {
+      delayed = true;
     },
-    title: {
-      display: false
+    delay: (context) => {
+      let delay = 0;
+      if (context.type === 'data' && context.mode === 'default' && !delayed) {
+        delay = context.dataIndex * 300 + context.datasetIndex * 100;
+      }
+      return delay;
+    },
+    
+  };
+
+
+  const config = {  
+    responsive:true,
+    animation,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: false
+      }
     }
+  };
+
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: data_,
+    options: config
+  });
+
+}
+
+
+
+window.addEventListener('scroll', () => {
+  let scrollPage = window.pageYOffset;
+  let portafolio = document.getElementById('portafolio').offsetTop;
+
+  if(scrollPage > portafolio - 400){
+    grafica();
   }
-};
-
-var myChart = new Chart(ctx, {
-  type: 'bar',
-  data: data_,
-  options: config
-});
-
+})
